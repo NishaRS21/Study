3225f1c765ef7d1a8eee947b0d9e944db5411283
NishaRs21 <nisha198420121999@gmail.com>
2021-11-29 23:42:49 +0530

    Add Tree view and nullable constraint

--- book/models/book_property.py
+++ book/models/book_property.py
@@ -5,7 +5,7 @@ class Author(models.Model):
     _name = 'author'
     _description = 'Author'
 
-    author_name = fields.Char()
+    author_name = fields.Char(required=True)
     address = fields.Text()
 
 
@@ -13,28 +13,28 @@ class BookDepartment(models.Model):
     _name = 'book.department'
     _description = 'Book Department'
 
-    name = fields.Char()
+    department_name = fields.Char(required=True)
 
 
 class BookCategory(models.Model):
     _name = 'book.category'
     _description = 'Book Category'
 
-    name = fields.Char()
+    Category_name = fields.Char(required=True)
 
 
 class BookPublisher(models.Model):
     _name = 'book.publisher'
     _description = 'Book Publisher'
 
-    name = fields.Char()
+    publisher_name = fields.Char(required=True)
 
 
 class Rack(models.Model):
     _name = 'rack'
     _description = 'Rack'
 
-    name = fields.Char()
+    name = fields.Char(required=True)
 
 
 # sheld_ids = fields.One2many('rack', 'rack_id')
@@ -44,7 +44,7 @@ class Shelf(models.Model):
     _name = 'shelf'
     _description = 'Shelf'
 
-    name = fields.Char()
+    name = fields.Char(required=True)
     rack_id = fields.Many2one('rack')
 
 
@@ -55,7 +55,7 @@ class bookProperty(models.Model):
 
     name = fields.Char()
     price = fields.Float()
-    author = fields.Char()
+    author = fields.Many2many('author_name')
     author_ids = fields.Many2many('author')
     isbn = fields.Integer()
     category_id = fields.Many2one('book.category')
